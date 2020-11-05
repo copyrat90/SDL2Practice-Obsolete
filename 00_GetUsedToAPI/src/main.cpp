f7303523940f663e976abfe7f99d516aaf996df1
copyrat90 <copyrat90@gmail.com>
2020-11-05 22:55:37 +0900

    201105 API Practice

--- 00_GetUsedToAPI/src/main.cpp
+++ 00_GetUsedToAPI/src/main.cpp
@@ -2,6 +2,9 @@
 #include <SDL.h>
 #include <SDL_image.h>
 
+constexpr int FPS = 60;
+constexpr int TICKS_PER_FRAME = 1000 / FPS;
+
 constexpr int WINDOW_WIDTH = 640;
 constexpr int WINDOW_HEIGHT = 480;
 
@@ -27,16 +30,49 @@ int main(int argc, char* args[])
         pTempSurface = nullptr;
     }
     
-    SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
-    SDL_RenderClear(g_pRenderer);
+    bool bRunning = true;
+    const SDL_Rect dstRect = {WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
+    SDL_Point rotateCenter = {dstRect.w * 3 / 4, dstRect.h / 4};
+    
+    double rotateAngle = 0;
 
-    SDL_Rect dstRect = {WINDOW_WIDTH / 4, WINDOW_HEIGHT / 4, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2};
-    SDL_RenderCopyEx(g_pRenderer, pHelloWorld, nullptr, &dstRect, 10, nullptr, SDL_FLIP_NONE);
+    while (bRunning)
+    {
+        const Uint32 startTicks = SDL_GetTicks();
 
-    SDL_RenderPresent(g_pRenderer);
+        SDL_Event e;
+        while (SDL_PollEvent(&e))
+        {
+            if (SDL_EventType::SDL_QUIT == e.type)
+            {
+                bRunning = false;
+            }
+        }
+
+        const bool* keyboardStates = (const bool*)SDL_GetKeyboardState(nullptr);
+        rotateAngle -= (keyboardStates[SDL_Scancode::SDL_SCANCODE_A]) ? 4 : 0;
+        rotateAngle += (keyboardStates[SDL_Scancode::SDL_SCANCODE_D]) ? 4 : 0;
+        rotateCenter.y -= (keyboardStates[SDL_SCANCODE_W]) ? 4 : 0;
+        rotateCenter.y += (keyboardStates[SDL_SCANCODE_S]) ? 4 : 0;
 
 
-    SDL_Delay(2000);
+        SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+        SDL_RenderClear(g_pRenderer);
+    
+        SDL_RenderCopyEx(g_pRenderer, pHelloWorld, nullptr, &dstRect, rotateAngle, &rotateCenter, SDL_FLIP_NONE);
+
+        SDL_SetRenderDrawColor(g_pRenderer, 255,0,0,0);
+        SDL_Rect rotateCenterRect = {dstRect.x + rotateCenter.x - 5, dstRect.y + rotateCenter.y - 5, 10, 10};
+        SDL_RenderFillRect(g_pRenderer, &rotateCenterRect);
+
+        SDL_RenderPresent(g_pRenderer);
+
+        const Uint32 elapsedTicks = SDL_GetTicks() - startTicks;
+        if (elapsedTicks < TICKS_PER_FRAME)
+        {
+            SDL_Delay(TICKS_PER_FRAME - elapsedTicks);
+        }
+    }
     
     clean();
     return 0;
